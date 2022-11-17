f1afd38f4bcc3c5c5b09737911c59fdd4e885b73
ProSweeper <dylan.doucet28@gmail.com>
2022-11-17 22:13:26 +0000

    connect Set and Swag models

--- main_app/models.py
+++ main_app/models.py
@@ -24,6 +24,8 @@ class Swag(models.Model):
   item = models.CharField(max_length=100)
   type = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
+  # create the relationship between sets and swag (M:M)
+  sets = models.ManyToManyField(Set)
 
   def __str__(self):
     return f'{self.item}, item id: {self.id}'
